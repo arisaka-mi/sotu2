aff8cd3dcb20b43bf0a783ddbba9e6e7377fea60
nakamichi <k023c2144@g.neec.ac.jp>
2025-11-18 10:26:11 +0900

    11/18

--- sotu2/web/navigation/nav.php
+++ sotu2/web/navigation/nav.php
@@ -1,10 +1,10 @@
-<!-- nav.php (共通ナビゲーション部分) -->
+
 <nav>
     <ul style="list-style:none; display:flex; gap:20px;">
-        <li><a href="index.php">🏠 タイムライン</a></li>
-        <li><a href="post.php">✍ 投稿する</a></li>
-        <li><a href="profile.php">👤 プロフィール</a></li>
-        <li><a href="logout.php">🚪 ログアウト</a></li>
+        <li><a href="timeline.php"><img src=""alt="ホーム画面"></a></li>
+        <li><a href="serch.php"><img src=""alt="検索画面"></a></li>
+        <li><a href="notify.php"><img src=""alt="通知画面"></a></li>
+        <li><a href="profile.php"><img src=""alt="プロフィール画面"></a></li>
     </ul>
 </nav>
 <hr>
