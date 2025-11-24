f9afe6bf986716772e4441c69472b5020bd0463c
arisaka-mi <k024c2048@g.neec.ac.jp>
2025-11-24 14:25:27 +0900

    11/24-14:25

---
+++ sotu2/MySQL/Comment.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Comment (
+    cmt_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    post_id INT(11) NOT NULL,
+    user_id INT(11) NOT NULL,
+    cmt TEXT NOT NULL,
+    cmt_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    parent_cmt_id INT DEFAULT NULL,
+    FOREIGN KEY (post_id) REFERENCES Post(post_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (parent_cmt_id) REFERENCES Comment(cmt_id) ON DELETE CASCADE
+);
