3ee506bc5d1d461646077fd9f4c10a43609ad4a5
arisaka-mi <k024c2048@g.neec.ac.jp>
2025-12-08 13:06:48 +0900

    12/8-13:06

--- sotu2/web/navigation/nav.php
+++ sotu2/web/navigation/nav.php
@@ -1,8 +1,7 @@
-
 <nav>
     <ul style="list-style:none; display:flex; gap:20px;">
         <li><a href="timeline.php"><img src=""alt="ホーム画面"></a></li>
-        <li><a href="serch.php"><img src=""alt="検索画面"></a></li>
+        <li><a href="search.php"><img src=""alt="検索画面"></a></li>
         <li><a href="notify.php"><img src=""alt="通知画面"></a></li>
         <li><a href="profile.php"><img src=""alt="プロフィール画面"></a></li>
     </ul>
