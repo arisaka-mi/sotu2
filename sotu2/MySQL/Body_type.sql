c13177247d8ddb8623c5595977b6fc3bf7f92989
arisaka-mi <k024c2048@g.neec.ac.jp>
2025-12-12 23:11:34 +0900

    12/12-23:11

--- sotu2/MySQL/Body_type.sql
+++ sotu2/MySQL/Body_type.sql
@@ -4,4 +4,7 @@ create table Body_type (
     bt_num int(10)
 );
 
-INSERT INTO `body_type` (`bt_id`, `bt_name`, `bt_num`) VALUES ('BT1', '骨格ストレート', NULL), ('BT2', '骨格ウェーブ', NULL), ('BT3', '骨格ナチュラル', NULL);
+INSERT INTO `body_type` (`bt_id`, `bt_name`, `bt_num`) VALUES 
+('BT1', '骨格ストレート', NULL), 
+('BT2', '骨格ウェーブ', NULL), 
+('BT3', '骨格ナチュラル', NULL);
--- sotu2/MySQL/Parsonal_color.sql
+++ sotu2/MySQL/Parsonal_color.sql
@@ -4,4 +4,8 @@ create table Parsonal_color (
     pc_num int(10)
 );
 
-INSERT INTO `parsonal_color` (`pc_id`, `pc_name`, `pc_num`) VALUES ('PC1', 'イエベ春', NULL), ('PC2', 'ブルべ夏', NULL), ('PC3', 'イエベ秋', NULL), ('PC4', 'ブルべ冬', NULL);
+INSERT INTO `parsonal_color` (`pc_id`, `pc_name`, `pc_num`) VALUES 
+('PC1', 'イエベ春', NULL), 
+('PC2', 'ブルべ夏', NULL), 
+('PC3', 'イエベ秋', NULL), 
+('PC4', 'ブルべ冬', NULL);
